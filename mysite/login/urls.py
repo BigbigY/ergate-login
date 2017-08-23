79d5cb265ad11e6a2996cb830f25dd80a6d15146
luck <yy520it@gmail.com>
2017-08-23 13:13:19 +0800

    修改后台账号验证

--- mysite/login/urls.py
+++ mysite/login/urls.py
@@ -3,7 +3,6 @@ from login import views
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^login/$',views.login,name = 'login'),
-    url(r'^regist/$',views.regist,name = 'regist'),
     url(r'^index/$',views.index,name = 'index'),
     url(r'^logout/$',views.logout,name = 'logout'),
 ]
--- mysite/login/views.py
+++ mysite/login/views.py
@@ -2,64 +2,34 @@
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext
-from django import forms
-from login.models import User
-
-
-#表单
-class UserForm(forms.Form): 
-    username = forms.CharField(label='用户名',max_length=100)
-    password = forms.CharField(label='密码',widget=forms.PasswordInput())
-
-
-#注册
-def regist(req):
-    if req.method == 'POST':
-        uf = UserForm(req.POST)
-        if uf.is_valid():
-            #获得表单数据
-            username = uf.cleaned_data['username']
-            password = uf.cleaned_data['password']
-            #添加到数据库
-            User.objects.create(username=username,password=password)
-            return HttpResponse('regist success!!')
-    else:
-        uf = UserForm()
-    return render_to_response('regist.html',{'uf':uf})
+from django.contrib import auth
+import importlib
 
 #登陆
-def login(req):
-    if req.method == 'POST':
-        uf = UserForm(req.POST)
-        if uf.is_valid():
-            #获取表单用户密码
-            #username = uf.cleaned_data['username']
-            #password = uf.cleaned_data['password']
-            username = req.POST.get('username')
-            password = req.POST.get('password')
-            #获取的表单数据与数据库进行比较
-            user = User.objects.filter(username__exact=username,password__exact=password)
-            if user:
-                #比较成功，跳转index
-                response = HttpResponseRedirect('/login/index/')
-                #将username写入浏览器cookie,失效时间为3600
-                response.set_cookie('username',username,3600)
-                return response
-            else:
-                #比较失败，还在login
-                return HttpResponseRedirect('/login/login/')
-    else:
-        uf = UserForm()
-    return render_to_response('login.html',{'uf':uf})
+def login(request):
+    user = request.user
+    #redirect_uri = request.GET.get('next')
+    redirect_uri = "index"
+    if request.method == 'POST':
+        global username
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        code = request.POST.get('code')
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user) 
+            return HttpResponseRedirect(redirect_uri)
+        else:
+            passwd_err = "用户名或密码错误！"
+            return render_to_response('login.html',locals())
+    return render_to_response('login.html')
 
 #登陆成功
-def index(req):
-    username = req.COOKIES.get('username','')
+def index(request):
     return render_to_response('index.html' ,{'username':username})
 
 #退出
-def logout(req):
-    response = HttpResponse('logout !!')
+def logout(request):
     #清理cookie里保存username
     response.delete_cookie('username')
-    return response
+    return HttpResponseRedirect('login')
