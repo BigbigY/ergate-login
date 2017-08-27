925f75ad20ae122db985a67c84e440bf876f8c26
luck <yy520it@gmail.com>
2017-08-27 23:05:23 +0800

    限制必须登陆，修改获取用户方法

--- mysite/login/urls.py
+++ mysite/login/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url, include
 from login import views
 urlpatterns = [
     url(r'^$', views.login, name='login'),
-    url(r'^login/$',views.login,name = 'login'),
     url(r'^index/$',views.index,name = 'index'),
     url(r'^logout/$',views.logout,name = 'logout'),
 ]
--- mysite/login/views.py
+++ mysite/login/views.py
@@ -3,18 +3,16 @@ from django.shortcuts import render,render_to_response
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 import importlib
 
 #登陆
 def login(request):
     user = request.user
-    #redirect_uri = request.GET.get('next')
     redirect_uri = "index"
     if request.method == 'POST':
-        global username
         username = request.POST.get('username')
         password = request.POST.get('password')
-        code = request.POST.get('code')
         user = auth.authenticate(username=username,password=password)
         if user is not None:
             auth.login(request,user) 
@@ -25,9 +23,11 @@ def login(request):
     return render_to_response('login.html')
 
 #登陆成功
+@login_required
 def index(request):
+    username = request.user
     return render_to_response('index.html' ,{'username':username})
 
 #退出
 def logout(request):
-    return HttpResponseRedirect('login')
+    return HttpResponseRedirect('/login/')
